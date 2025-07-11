8597db0dc1d90926cedc663dedbaab976d9d139c
willmich2 <willmich@mit.edu>
2025-07-11 11:30:35 -0400

    created function for field propagation for use in forward models

--- src/forwardmodels.py
+++ src/forwardmodels.py
@@ -5,7 +5,6 @@ from src.elements import ArbitraryElement
 from src.simparams import SimParams
 from src.montecarlo import mc_propagate
 
-
 def propagate_z_arbg_z(
     U: torch.Tensor, 
     z: float, 
@@ -20,17 +19,14 @@ def propagate_z_arbg_z(
     Uzgz = propagate_z(Uzg, z, sim_params)
     return Uzgz
 
-
-def forward_model_focus_plane_wave(
-    x: torch.Tensor, 
+def field_z_arbg_z(
+    x: torch.Tensor,
     sim_params: SimParams,
     elem_params: dict,
-    forward_model_args: dict,
     z: float, 
-    ) -> float:
+    ) -> torch.Tensor:
     """
     Propagate a plane wave a distance z, apply an arbitrary element, and propagate a distance z again.
-    Then, calculate the visibility of the output field.
     """
     element = ArbitraryElement(
         name="ArbitraryElement", 
@@ -49,6 +45,21 @@ def forward_model_focus_plane_wave(
         z=z, 
         sim_params=sim_params
         )
+
+    return U_out_mc
+
+def forward_model_focus_plane_wave(
+    x: torch.Tensor, 
+    sim_params: SimParams,
+    elem_params: dict,
+    forward_model_args: dict,
+    z: float, 
+    ) -> float:
+    """
+    Propagate a plane wave a distance z, apply an arbitrary element, and propagate a distance z again.
+    Then, calculate the visibility of the output field.
+    """
+    U_out_mc = field_z_arbg_z(x, sim_params, elem_params, z)
 
     Ncenter = forward_model_args["Ncenter"]
     Navg = forward_model_args["Navg"]
@@ -74,23 +85,7 @@ def forward_model_focus_plane_wave_power(
     Propagate a plane wave a distance z, apply an arbitrary element, and propagate a distance z again.
     Then, calculate the power within a center region of the output field.
     """
-    element = ArbitraryElement(
-        name="ArbitraryElement", 
-        thickness=elem_params["thickness"], 
-        n_elem=elem_params["n_elem"], 
-        n_gap=elem_params["n_gap"], 
-        x=x
-    )
-
-    U_out_mc = mc_propagate(
-        u_init_func=plane_wave, 
-        u_init_func_args=(), # no arguments for plane wave
-        prop_func=propagate_z_arbg_z,
-        prop_func_args=(element,),
-        n=1,
-        z=z, 
-        sim_params=sim_params
-        )
+    U_out_mc = field_z_arbg_z(x, sim_params, elem_params, z)
 
     Ncenter = forward_model_args["Ncenter"]
 
