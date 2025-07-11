586270d1973cfeb5ce02ca47a062e2ef45771622
willmich2 <willmich@mit.edu>
2025-07-11 11:22:06 -0400

    added central power forward model

--- src/forwardmodels.py
+++ src/forwardmodels.py
@@ -62,3 +62,42 @@ def forward_model_focus_plane_wave(
     obj = visibility
 
     return obj
+
+def forward_model_focus_plane_wave_power(
+    x: torch.Tensor, 
+    sim_params: SimParams,
+    elem_params: dict,
+    forward_model_args: dict,
+    z: float, 
+    ) -> float:
+    """
+    Propagate a plane wave a distance z, apply an arbitrary element, and propagate a distance z again.
+    Then, calculate the power within a center region of the output field.
+    """
+    element = ArbitraryElement(
+        name="ArbitraryElement", 
+        thickness=elem_params["thickness"], 
+        n_elem=elem_params["n_elem"], 
+        n_gap=elem_params["n_gap"], 
+        x=x
+    )
+
+    U_out_mc = mc_propagate(
+        u_init_func=plane_wave, 
+        u_init_func_args=(), # no arguments for plane wave
+        prop_func=propagate_z_arbg_z,
+        prop_func_args=(element,),
+        n=1,
+        z=z, 
+        sim_params=sim_params
+        )
+
+    Ncenter = forward_model_args["Ncenter"]
+
+    I_out = U_out_mc.abs().pow(2).reshape(sim_params.Nx)
+    
+    P_out_center = I_out[I_out.shape[0]//2 - Ncenter//2:I_out.shape[0]//2 + Ncenter//2].sum()
+
+    obj = P_out_center
+
+    return obj
