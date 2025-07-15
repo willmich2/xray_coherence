29524789014c0fb99f2d1eb32d44633e72c95828
willmich2 <willmich@mit.edu>
2025-07-15 10:16:17 -0400

    added option for uniform wavelength for kramers law

--- src/util.py
+++ src/util.py
@@ -4,7 +4,8 @@ from typing import Tuple
 def kramers_law_weights(
         e_min: float,
         e_max: float,
-        N: int
+        N: int,
+        uniform_energy: bool = True # if True, the energy is sampled uniformly, otherwise the wavelength is sampled uniformly
 ) -> Tuple[np.ndarray, np.ndarray]: 
     """
     Calculate the spectral weights according to Kramer's law. The input energies are in eV and 
@@ -14,10 +15,17 @@ def kramers_law_weights(
     h = 4.135667696e-15 # eV s
     c = 299792458 # m/s
     lam_min = h * c / e_max
-
-    energies  = np.linspace(e_min, e_max, N)
-    lams = h * c / energies
-    weights = (lams / lam_min - 1) / lams**2
+    lam_max = h * c / e_min
+    
+    # if uniform_energy is True, the energy is sampled uniformly, otherwise the wavelength is sampled uniformly
+    if uniform_energy:
+        energies  = np.linspace(e_min, e_max, N)
+        lams = h * c / energies
+        weights = (lams / lam_min - 1) / lams**2
+    else:
+        lams = np.linspace(lam_min, lam_max, N)
+        energies = h * c / lams
+        weights = (energies / e_min - 1) / energies**2
     # ensure weights sum to 1
     weights /= np.sum(weights)
     return lams, weights
