fixed weighted intensity calculation

src/forwardmodels.py
@@ -42,17 +42,14 @@ def field_z_arbg_z(
         x=x
     )
 
-    U_out_mc = mc_propagate(
-        u_init_func=plane_wave, 
-        u_init_func_args=(), # no arguments for plane wave
-        prop_func=propagate_z_arbg_z,
-        prop_func_args=(element,),
-        n=1,
-        z=z, 
-        sim_params=sim_params
+    Uzgz = propagate_z_arbg_z(
+        U = plane_wave(sim_params), 
+        z = z, 
+        sim_params = sim_params, 
+        element = element
         )
 
-    return U_out_mc
+    return Uzgz
 
 def forward_model_focus_plane_wave(
     x: torch.Tensor, 
@@ -69,9 +66,9 @@ def forward_model_focus_plane_wave(
     """
     n = opt_params["n"]
     x_opt = torch.repeat_interleave(x, n)
-    U_out_mc = field_z_arbg_z(x_opt, sim_params, elem_params, z)
+    Uzgz = field_z_arbg_z(x_opt, sim_params, elem_params, z)
 
-    I_out = U_out_mc.abs().pow(2).reshape(sim_params.Nx)
+    I_out = torch.sum(Uzgz.abs().pow(2) * sim_params.weights, dim=0).reshape(sim_params.Nx)
     
     I_out_center = I_out[I_out.shape[0]//2 - Ncenter//2:I_out.shape[0]//2 + Ncenter//2].mean()
     I_out_edge = torch.cat((I_out[:Navg], I_out[-Navg:])).mean()
@@ -99,9 +96,10 @@ def forward_model_focus_plane_wave_power(
     n = opt_params["n"]
     x_opt = torch.repeat_interleave(x_dbl, n)
 
-    U_out_mc = field_z_arbg_z(x_opt, sim_params, elem_params, z)
+    Uzgz = field_z_arbg_z(x_opt, sim_params, elem_params, z)
 
-    I_out = U_out_mc.abs().pow(2).reshape(sim_params.Nx)
+    # calculated intensity by summing over wavelengths, weighted by weights
+    I_out = torch.sum(Uzgz.abs().pow(2) * sim_params.weights, dim=0).reshape(sim_params.Nx)
     
     P_out_center = I_out[I_out.shape[0]//2 - Ncenter//2:I_out.shape[0]//2 + Ncenter//2].sum()
 
@@ -123,9 +121,9 @@ def forward_model_focus_plane_wave_overlap(
     """
     n = opt_params["n"]
     x_opt = torch.repeat_interleave(x, n)
-    U_out_mc = field_z_arbg_z(x_opt, sim_params, elem_params, z)
+    Uzgz = field_z_arbg_z(x_opt, sim_params, elem_params, z)
 
-    I_out = U_out_mc.abs().pow(2).reshape(sim_params.Nx)
+    I_out = torch.sum(Uzgz.abs().pow(2) * sim_params.weights, dim=0).reshape(sim_params.Nx)
 
     R = torch.sqrt(sim_params.X**2 + sim_params.Y**2)
     I_focus = torch.exp(-R**2 / (2 * r_focus**2))

src/propagation.py
@@ -38,11 +38,11 @@ def propagate_z(
     sim_params: SimParams
     ) -> torch.Tensor:
     
-    Uz = torch.zeros((sim_params.Ny, sim_params.Nx), dtype=torch.complex64, device=sim_params.device)
-    
-    for weight, lam in zip(sim_params.weights, sim_params.lams):
-        Uz_lam = angular_spectrum_propagation(U, lam, z, sim_params.dx, sim_params.device)
-        Uz += Uz_lam*weight
+    Uz = torch.zeros((len(sim_params.weights), sim_params.Ny, sim_params.Nx), dtype=torch.complex64, device=sim_params.device)
+
+    for i, lam in enumerate(sim_params.lams):
+        Uz_lam = angular_spectrum_propagation(U[i, :, :], lam, z, sim_params.dx, sim_params.device)
+        Uz[i, :, :] = Uz_lam
     return Uz
 
 
@@ -102,12 +102,12 @@ def apply_element(
     element, 
     sim_params: SimParams
     ) -> torch.Tensor:
-    U_f = torch.zeros((sim_params.Ny, sim_params.Nx), dtype=torch.complex64, device=sim_params.device)
+    U_f = torch.zeros((len(sim_params.weights), sim_params.Ny, sim_params.Nx), dtype=torch.complex64, device=sim_params.device)
     
-    for weight, lam in zip(sim_params.weights, sim_params.lams):
+    for i, lam in enumerate(sim_params.lams):
         transmission = element.transmission(lam, element.n_elem, element.n_gap, sim_params)
-        U_lam = U * transmission
-        U_f += U_lam * weight
+        U_lam = U[i, :, :] * transmission
+        U_f[i, :, :] = U_lam
     return U_f
 
 