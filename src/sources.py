979767748be407c9e9e8dc69a09697819aa78e35
willmich2 <willmich@mit.edu>
2025-07-18 12:25:18 -0400

    fixed shapes of source tensors

--- src/sources.py
+++ src/sources.py
@@ -2,15 +2,15 @@ import torch # type: ignore
 from src.simparams import SimParams
 torch.pi = torch.acos(torch.zeros(1)).item() * 2
 
-def gaussian_source(params: SimParams, rsrc: float) -> torch.Tensor:
+def gaussian_source(sim_params: SimParams, rsrc: float) -> torch.Tensor:
   # Gaussian source
-    U_init = torch.zeros((params.Ny, params.Nx), dtype=torch.complex64, device=params.device)
-    R_sq = params.X**2 + params.Y**2
-    U_init = torch.exp(-R_sq / (2 * rsrc**2)) * torch.exp(1j * 2*torch.pi * torch.rand(U_init.shape, dtype=torch.float32, device=params.device))
+    U_init = torch.zeros((len(sim_params.weights), sim_params.Ny, sim_params.Nx), dtype=torch.complex64, device=sim_params.device)
+    R_sq = sim_params.X**2 + sim_params.Y**2
+    U_init = torch.exp(-R_sq / (2 * rsrc**2)) * torch.exp(1j * 2*torch.pi * torch.rand(U_init.shape, dtype=torch.float32, device=sim_params.device))
 
     return U_init
 
-def plane_wave(params: SimParams) -> torch.Tensor:
-    U_init = torch.ones((params.Ny, params.Nx), dtype=torch.complex64, device=params.device)
+def plane_wave(sim_params: SimParams) -> torch.Tensor:
+    U_init = torch.ones((len(sim_params.weights), sim_params.Ny, sim_params.Nx), dtype=torch.complex64, device=sim_params.device)
 
     return U_init
