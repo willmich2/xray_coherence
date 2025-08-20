4ca323b82b611ac1c42bc280bb20073d6d841bf5
willmich2 <willmich@mit.edu>
2025-08-20 11:47:28 -0400

    added  mc_propagate_accumulate_intensity

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -23,3 +23,28 @@ def mc_propagate(
 
         mc_tensor[i, :, :, :] = u_final
     return mc_tensor
+
+
+def mc_propagate_accumulate_intensity(
+    u_init_func: Callable, 
+    u_init_func_args: tuple, 
+    prop_func: Callable, 
+    prop_func_args: tuple, 
+    n: int, 
+    z: float, 
+    sim_params: SimParams
+    ) -> torch.Tensor:
+    """
+    Repeatedly propagate an initial field given by u_init_func through a propagation function prop_func.
+    Returns a tensor of shape (Nwvl, Ny, Nx) where n is the number of Monte Carlo samples.
+    """
+    i_final = torch.zeros((sim_params.weights.shape[0], sim_params.Ny, sim_params.Nx), dtype=sim_params.dtype, device=sim_params.device)
+
+    for i in range(n):
+        u_init = u_init_func(sim_params, *u_init_func_args)
+        u_final = prop_func(u_init, z, sim_params, *prop_func_args)
+        i_final += torch.abs(u_final)**2
+
+    i_final /= n
+    
+    return i_final
