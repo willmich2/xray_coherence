e89f3287453b94f3e270344c216e33c9d8c7430e
willmich2 <willmich@mit.edu>
2025-08-21 11:03:30 -0400

    added assertion to MC propagation

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -45,6 +45,7 @@ def mc_propagate_accumulate_intensity(
         for wvl in range(sim_params.weights.shape[0]):
             sim_params.lams = sim_params.lams[wvl]
             u_init = u_init_func(sim_params, *u_init_func_args)
+            assert u_init.shape[0] == 1, "u_init must be a single wavelength"
             u_final = prop_func(u_init, z, sim_params, *prop_func_args)
             i_i += torch.abs(u_final)**2 * sim_params.weights[wvl]
 
